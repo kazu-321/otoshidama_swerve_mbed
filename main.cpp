3171d3456fcee004cf7ae543def72a6b479b09db
kazu-321 <kzs321kzs@gmail.com>
2025-04-09 09:56:09 +0900

    feat: calculate some pid

--- main.cpp
+++ main.cpp
@@ -1,10 +1,23 @@
 #include "mbed.h"
+#include "robomas.h"
 
-// main() runs in its own thread in the OS
-int main()
-{
-    while (true) {
+robomas bus0(PB_8,PB_9);
+robomas bus1(PB_5,PB_6);
+robomas bus2(PA_8,PA_15);
+Ticker robomas_pid;
+void calculate_pid();
+const int dt = 10; // milliseconds
 
+
+int main() {
+    robomas_pid.attach(&calculate_pid,chrono::milliseconds(dt));
+    while(true) {
+        
     }
 }
 
+void calculate_pid() {
+    bus0.calculate_pid(dt);
+    bus1.calculate_pid(dt);
+    bus2.calculate_pid(dt);
+}
